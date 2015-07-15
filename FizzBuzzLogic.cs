115fb7f10ea0131f586594bed976512195236778
Travis K <traviskean04@live.com>
2015-07-15 16:03:37 -0400

    Demo of FizzBuzz to test

---
+++ FizzBuzzLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_TDD_Demo
+{
+    public class FizzBuzzLogic
+    {
+        public string DoTheThing(int num)
+        {
+            string answer = "";
+            if (num % 3 != 0 && num % 5 != 0)
+                answer = num.ToString();
+            else
+                answer = "no";
+
+            return answer;
+        }
+    }
+}
